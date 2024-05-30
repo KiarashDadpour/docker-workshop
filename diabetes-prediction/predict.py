a87ca46433ea92df6d1fc835bffddba790accf65
Kiarash Dadpour <140727992+KiarashDadpour@users.noreply.github.com>
2024-05-30 22:51:43 +0330

    Update predict.py

--- diabetes-prediction/predict.py
+++ diabetes-prediction/predict.py
@@ -1,1 +1,31 @@
+import pickle
+
+# Load the model
+with open("logreg.pkl", "rb") as pickle_in:
+    model = pickle.load(pickle_in)
+
+
+# Function to get user input and make prediction
+def get_user_input():
+    try:
+        # Prompt the user to enter values separated by comma
+        input_string = input("Enter values for the features separated by spaces: ")
+
+        # Split the input string into individual values
+        input_values = list(map(float, input_string.split(',')))
+
+        # Ensure that exactly 8 values are provided
+        if len(input_values) != 8:
+            print("Please enter exactly 8 values.")
+            return
+
+        # Make prediction
+        prediction = model.predict([input_values])
+        print(f"The prediction is: {prediction[0]}")
+
+    except ValueError:
+        print("Please enter valid numerical values.")
+
+
+get_user_input()
 
