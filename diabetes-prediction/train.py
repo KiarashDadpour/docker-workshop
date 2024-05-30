a054e567c06efe1d3346b2c2ba7f378ae63f71dc
Kiarash Dadpour <140727992+KiarashDadpour@users.noreply.github.com>
2024-05-30 22:48:53 +0330

    Update train.py

--- diabetes-prediction/train.py
+++ diabetes-prediction/train.py
@@ -1,1 +1,39 @@
+import pandas as pd
+
+df = pd.read_csv('diabetes.csv')
+# print(df.shape)
+# print(df.head())
+# print(df.Outcome.value_counts())
+# print(df.info())
+input_columns = [column for column in df.columns if column != "Outcome"]
+# print(input_columns)
+output_colmun = "Outcome"
+print(output_colmun)
+
+X = df.loc[:, input_columns].values
+y = df.loc[:, output_colmun]
+# print(X.shape, Y.shape)
+
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=555, stratify=y)
+
+# print(np.sum(y_train))
+# print(np.sum(y_test))
+
+from sklearn.linear_model import LogisticRegression
+
+logreg = LogisticRegression(class_weight='balanced', max_iter=1000).fit(X_train, y_train)
+# print(logreg.score(X_test, y_test))
+
+predictions = logreg.predict(X_test)
+
+# print(classification_report(y_test, predictions, target_names=["Non Outcome", "Outcome"]))
+
+import pickle
+
+pickle_out = open("logreg.pkl", "wb")
+pickle.dump(logreg, pickle_out)
+pickle_out.close()
 
