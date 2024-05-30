5ba0dfe3f126f8096eee90cba8a70bb6a06140fa
Kiarash Dadpour <140727992+KiarashDadpour@users.noreply.github.com>
2024-05-30 23:08:39 +0330

    Create main.py

---
+++ crypto-price/main.py
@@ -0,0 +1,21 @@
+import requests
+import pandas as pd
+
+url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,dogecoin,tether&vs_currencies=usd"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+
+    crypto_data = []
+    for crypto, price_data in data.items():
+        usd_price = price_data["usd"]
+        formatted_price = "${:,.2f}".format(usd_price)
+        crypto_data.append({"Crypto": crypto.upper(), "USD Price": formatted_price})
+
+    df = pd.DataFrame(crypto_data)
+
+    print(df.to_string(index=False))
+else:
+    print("Error:", response.status_code)
