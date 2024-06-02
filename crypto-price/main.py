308cfed1675a15635b379024833088d201901464
Kiarash Dadpour <140727992+KiarashDadpour@users.noreply.github.com>
2024-06-02 21:16:59 +0330

    Update main.py

--- crypto-price/main.py
+++ crypto-price/main.py
@@ -12,7 +12,7 @@ if response.status_code == 200:
     for crypto, price_data in data.items():
         usd_price = price_data["usd"]
         formatted_price = "${:,.2f}".format(usd_price)
-        crypto_data.append({"Crypto ": crypto.upper(), "USD Price": formatted_price})
+        crypto_data.append({"Crypto ": crypto.upper(), "USD Price ": formatted_price})
 
     df = pd.DataFrame(crypto_data)
 
